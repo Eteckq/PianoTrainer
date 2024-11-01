e91e49dca1c05c4eedaca6889efb5af44f72ec6d
= <yohan.g99@gmail.com>
2024-11-01 19:34:10 +0100

    fix midi panel

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,27 +6,18 @@ const show = ref(false);
   <div
     class="flex flex-col transition-all duration-500 text-white h-screen overflow-hidden"
   >
-    <div class="absolute top-2 right-2 z-10 cursor-pointer" @click="show = !show"><span v-if="!show">Show</span> <span v-else>Hide</span> bottom bar</div>
+    <div
+      class="absolute top-2 right-2 z-10 cursor-pointer"
+      @click="show = !show"
+    >
+      <span v-if="!show">Show</span> <span v-else>Hide</span> bottom bar
+    </div>
 
     <Navbar />
     <div class="grow w-full">
       <slot />
     </div>
     <Piano />
-    <transition name="slide">
-      <BottomBar v-if="show" />
-    </transition>
+    <BottomBar class="transition-all duration-500" :class="{ 'h-0': !show }" />
   </div>
 </template>
-
-<style scoped>
-.slide-enter-active,
-.slide-leave-active {
-  transition: all 0.5s ease;
-}
-
-.slide-enter-from,
-.slide-leave-to {
-  height: 0;
-}
-</style>
